8bb13aed54f9261494945c1221f8b90dc7e9a417
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-06 01:08:27 +0300

    completed.

--- lib/testing/conftest.py
+++ lib/testing/conftest.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+# conftest.py
+import pytest
+from dog import engine, Base
 
-def pytest_itemcollected(item):
-    par = item.parent.obj
-    node = item.obj
-    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
-    suf = node.__doc__.strip() if node.__doc__ else node.__name__
-    if pref or suf:
-        item._nodeid = ' '.join((pref, suf))
+@pytest.fixture(scope="module")
+def setup_and_teardown():
+    # Setup
+    Base.metadata.create_all(bind=engine)
+    yield
+    # Teardown
+    Base.metadata.drop_all(bind=engine)
